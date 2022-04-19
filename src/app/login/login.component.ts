e39722bc4a46f9df67ef4a61701014902bcd97ba
Zach <zvr5084@psu.edu>
2022-04-19 12:18:00 -0400

    Finish Making Shopping Cart, Link account details to welcome page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,17 @@ import {Router} from "@angular/router";
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  userID = "Enter User ID";
+  userID = "";
   password = "";
   message = "";
+  message1 = "";
+  message2 = "";
+  message3 = "";
+  email1 = "";
+  passwordcreate = "";
+  lname = "";
+  fname = "";
+
   constructor(private router: Router) {}
   signIn() {
     if(this.userID == 'admin' && this.password == 'admin') {
@@ -17,6 +25,30 @@ export class LoginComponent implements OnInit {
     } else {
       this.message = 'Invalid login id or password';
     }
+  }
+  signUp() {
+    if(this.email1.includes("@") == true) {
+      if(this.passwordcreate.length < 8) {
+        this.message2 = "Password must be longer than 7 characters";
+      }
+      if(this.passwordcreate.length >= 8){
+        if (this.containsSpecialChars({str: this.passwordcreate})) {
+          this.router.navigateByUrl('/welcome').then(r => {});
+        }
+        else {
+          this.message3 = "Password must contain at least 1 special character";
+        }
+      }
+    }
+    else{
+      this.message1 = "Email address not valid";
+    }
+
+
+  }
+  containsSpecialChars({str}: { str: any }) {
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    return specialChars.test(str);
   }
 
   ngOnInit(): void {
